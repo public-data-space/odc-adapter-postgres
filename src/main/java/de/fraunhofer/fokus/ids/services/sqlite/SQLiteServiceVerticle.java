1ef602c071f34a7d4cae1f76dfb23da718ec939b
Vincent Bohlen <vincent.bohlen@fokus.fraunhofer.de>
2019-09-24 13:38:54 +0200

    Use passed repository env variable to init local db

--- src/main/java/de/fraunhofer/fokus/ids/services/sqlite/SQLiteServiceVerticle.java
+++ src/main/java/de/fraunhofer/fokus/ids/services/sqlite/SQLiteServiceVerticle.java
@@ -1,6 +1,5 @@
 package de.fraunhofer.fokus.ids.services.sqlite;
 
-import de.fraunhofer.fokus.ids.services.database.DatabaseService;
 import de.fraunhofer.fokus.ids.services.util.Constants;
 import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
@@ -32,7 +31,7 @@ public class SQLiteServiceVerticle extends AbstractVerticle {
             if (ar.succeeded()) {
                 JsonObject env = ar.result();
                 JsonObject config = new JsonObject()
-                        .put("url", "jdbc:sqlite:"+env.getString("DB_URL"))
+                        .put("url", "jdbc:sqlite:"+env.getString("REPOSITORY")+"db")
                         .put("driver_class", "org.sqlite.jdbcDriver")
                         .put("max_pool_size", 30);
                 SQLClient jdbc = JDBCClient.createShared(vertx, config);
